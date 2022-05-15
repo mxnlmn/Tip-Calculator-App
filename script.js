1f09da8fe6016d52dcb5d629802bec009e15eed4
Tymoshenko Volodymyr <71495910+mxnlmn@users.noreply.github.com>
2022-05-15 19:29:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,97 +1,47 @@
-let tipAmount = document.getElementById('tip_amount');
-let totalTip = document.getElementById('total_tip');
-
 const inputBill = document.getElementById('input_bill');
-const inputNumberPeople = document.getElementById('input_number_of_people');
 
-const fivePer = document.getElementById('5');
-const tenPer = document.getElementById('10');
-const fifteenthPer = document.getElementById('15');
-const twentyPer = document.getElementById('25');
-const fiftyPer = document.getElementById('50');
-const inputPer = document.getElementById('input_percentage');
+const tipBtn = Array.from(document.getElementsByClassName('percentage'))
+const tipInput = document.getElementById('input_percentage');
 
-const resetBtn = document.getElementById('reset_button');
+const peopleNumber = document.getElementById('input_number_of_people');
 
-let tip = 0;
-let calculationTipAmount;
-let total;
-let calculationTotal;
+const tipAmount = document.getElementById('tip_amount');
+const tipTotal = document.getElementById('total_tip');
 
-fivePer.addEventListener('click', function(){
-    tip = 5;
-    calculationTipAmount =  (tip * inputBill.value)/100;
-    tipAmount.innerHTML = inputBill.value + calculationTipAmount;
-    total = inputBill.value - calculationTipAmount;
-    inputPer.value = '';
+const resetBtn = document.getElementById('reset_button')
+inputBill.addEventListener('input', function(){
+    tipAmount.innerHTML = inputBill.value;
 })
-
-tenPer.addEventListener('click', function(){
-    tip = 10;
-    calculationTipAmount =  (tip * inputBill.value)/100;
-    tipAmount.innerHTML = inputBill.value + calculationTipAmount;
-    total = inputBill.value - calculationTipAmount;
-    inputPer.value = '';
-})
-fifteenthPer.addEventListener('click', function(){
-    tip = 15;
-    calculationTipAmount =  (tip * inputBill.value)/100;
-    tipAmount.innerHTML = inputBill.value + calculationTipAmount;
-    total = inputBill.value - calculationTipAmount;
-    inputPer.value = '';
-})
-twentyPer.addEventListener('click', function(){
-    tip = 20;
-    calculationTipAmount =  (tip * inputBill.value)/100;
-    tipAmount.innerHTML = inputBill.value + calculationTipAmount;
-    total = inputBill.value - calculationTipAmount;
-    inputPer.value = '';
-})
-fiftyPer.addEventListener('click', function(){
-    tip = 50;
-    calculationTipAmount =  (tip * inputBill.value)/100;
-    tipAmount.innerHTML = inputBill.value + calculationTipAmount;
-    total = inputBill.value - calculationTipAmount;
-    inputPer.value = '';
-})
-
-inputPer.addEventListener('input', function(){
-    tip = inputPer.value;
-    calculationTipAmount =  (tip * inputBill.value)/100;
-    tipAmount.innerHTML = inputBill.value + calculationTipAmount;
-    total = inputBill.value - calculationTipAmount;
-})
-
-if(inputBill.value == undefined){
-    tipAmount = '0';
-    
-}
-else{
-    inputBill.addEventListener('input',function(){
-        tipAmount.innerHTML = Math.round(inputBill.value);
-    })
-
-   
-}
-if(inputNumberPeople.value == undefined){
-    totalTip = '0';
-}
-else{
-    inputNumberPeople.addEventListener('input', function(){
-        totalTip.innerHTML = Math.round(inputNumberPeople.value * total);
+for(let i = 0; i < tipBtn.length; i++){
+    tipBtn[i].addEventListener('click', function(){
+        tipAmount.innerHTML = inputBill.value;
+        tipAmount.innerHTML = parseFloat(tipAmount.innerHTML) + (parseFloat(tipAmount.innerHTML)*parseFloat(tipBtn[i].innerHTML)/100)
     })
 }
+
+tipInput.addEventListener('input', function(){
+    tipAmount.innerHTML = inputBill.value;
+    tipAmount.innerHTML = parseFloat(tipAmount.innerHTML) + (parseFloat(tipAmount.innerHTML) * parseFloat(tipInput.value)/100);
+    if(tipInput.value == ''){
+        tipAmount.innerHTML = inputBill.value;
+    }
+})
+
+peopleNumber.addEventListener('input', function(){
+    if(peopleNumber == ''){
+        tipTotal.innerHTML = 0;
+    }
+    else{
+        tipTotal.innerHTML = parseFloat(peopleNumber.value) * parseFloat(tipAmount.innerHTML) 
+    }
+})
 
 resetBtn.addEventListener('click', function(){
-    tip = 0;
-    inputBill.value = 0;
-    inputNumberPeople.value = 0;
-    totalTip.innerHTML = 0;
+    inputBill.value = '';
+    tipInput.value = '';
+    peopleNumber.value = '';
+
     tipAmount.innerHTML = 0;
-    inputPer.value = '';
+    tipTotal.innerHTML = 0;
 
 })
-
-
-
-
